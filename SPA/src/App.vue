1a6bb5b075d60cb451814d917ff0a0cbfd042722
mkubincova <mkubincova@proton.me>
2024-04-16 14:50:44 +0200

    feat: refs & components

--- SPA/src/App.vue
+++ SPA/src/App.vue
@@ -1,15 +1,28 @@
 <template>
   <h1>{{title}}</h1>
+  <input type="text" ref="name">
+  <button @click="handleClick">Click me</button>
+  <Modal />
 </template>
 
 <script>
+import Modal from './components/Modal.vue'
+
 export default {
   name: 'App',
   data () {
     return {
       title: 'Hello there.'
     }
-  }
+  },
+  methods: {
+    handleClick() {
+      console.log(this.$refs.name);
+      this.$refs.name.classList.add('active')
+      this.$refs.name.focus()
+    }
+  },
+  components: { Modal }
 }
 </script>
 
---
+++ SPA/src/components/Modal.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="backdrop">
+        <div class="modal">
+            <p>modal content</p>
+        </div>
+    </div>
+</template>
+
+<style>
+    .modal {
+        width: 400px;
+        padding: 20px;
+        margin: 100px auto;
+        background: #fff;
+        border-radius: 10px;
+        color: #000;
+    }
+    .backdrop {
+        inset: 0;
+        position: fixed;
+        background: rgba(0,0,0,0.5);
+    }
+</style>
