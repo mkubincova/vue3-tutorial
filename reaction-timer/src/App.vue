39c4f806a9129252c45f7c23aa381460f1323be1
mkubincova <mkubincova@proton.me>
2024-04-17 13:45:59 +0200

    feat: timer finished

--- reaction-timer/src/App.vue
+++ reaction-timer/src/App.vue
@@ -1,10 +1,50 @@
-<script>
-</script>
-
 <template>
-  <h1>Hello world</h1>
+  <h1>Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">play</button>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame" />
+  <Results v-if="score" :score="score" />
 </template>
 
-<style>
+<script>
+import Block from "./components/Block.vue"
+import Results from "./components/Results.vue"
 
+export default {
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null
+    }
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000
+      this.isPlaying = true
+      this.score = null
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime
+      this.isPlaying = false
+    }
+  },
+  components: { Block, Results }
+}
+</script>
+
+<style>
+  button {
+    border: none;
+    padding: 8px 16px;
+    border-radius: 4px;
+    font-size: 16px;
+    letter-spacing: 1px;
+    cursor: pointer;
+    margin: 10px;
+    background: #40b681;
+  }
+  button[disabled] {
+    opacity: 0.2;
+    cursor: not-allowed;
+  }
 </style>
--- reaction-timer/src/components/Results.vue
+++ reaction-timer/src/components/Results.vue
@@ -1,4 +1,27 @@
 <template>
-
+  <p>Reaction time: {{ score }}ms</p>
+  <p class="rank">{{ rank }}</p>
 </template>
-  
+
+<script>
+export default {
+  props: ['score'],
+  data() {
+    return {
+      rank: null
+    }
+  },
+  mounted() {
+    if (this.score < 250) this.rank = 'You are speed ⚡'
+    else if(this.score < 400) this.rank = 'Not great, not terrible'
+    else this.rank = 'Sloth from Zootopia 🦥'
+  }
+}
+</script>
+
+<style>
+  .rank {
+    font-size: 1.4em;
+    font-weight: bold;
+  }
+</style>
