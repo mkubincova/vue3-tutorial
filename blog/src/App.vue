de294efe6b56571d0c17fcc30e399fa1ab147729
mkubincova <mkubincova@proton.me>
2024-04-22 09:50:27 +0200

    feat: composition api setup

---
+++ blog/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <nav>
+    <RouterLink to="/">Home</RouterLink>
+  </nav>
+
+  <RouterView />
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+
+</style>
---
+++ blog/src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    <h1>home</h1>
+    <p>{{ name }}</p>
+    <p>{{ age }}</p>
+    <button @click="handleClick">click me</button>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Home',
+    setup() {
+        let name = 'mario'
+        let age = 30
+
+        const handleClick = () => {
+            console.log('clicked');
+        }
+
+        return { name, age, handleClick }
+    }
+}
+</script>
+
+<style>
+
+</style>
