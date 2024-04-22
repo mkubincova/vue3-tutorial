564ca1f4a535c04dda59fc81ac5b82b5f2480bc1
mkubincova <mkubincova@proton.me>
2024-04-22 10:05:00 +0200

    feat: refs in setup()

--- blog/src/views/Home.vue
+++ blog/src/views/Home.vue
@@ -1,24 +1,32 @@
 <template>
   <div class="home">
     <h1>home</h1>
-    <p>{{ name }}</p>
+    <p ref="p">{{ name }}</p>
     <p>{{ age }}</p>
     <button @click="handleClick">click me</button>
+    <button @click="age++">Increase age</button>
+    <input type="text" v-model="name">
   </div>
 </template>
 
 <script>
+import { ref } from 'vue'
+
 export default {
     name: 'Home',
     setup() {
-        let name = 'mario'
-        let age = 30
+      const p = ref(null)
 
-        const handleClick = () => {
-            console.log('clicked');
-        }
+      const name = ref('mario')
+      const age = ref(30)
 
-        return { name, age, handleClick }
+      const handleClick = () => {
+          console.log(p, p.value) // <p>...
+          name.value = 'luigi'
+          age.value = 35
+      }
+
+      return { name, age, handleClick, p }
     }
 }
 </script>
