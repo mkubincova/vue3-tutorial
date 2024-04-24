67429f63ea5d223560a8a301dee2d7cd1370b452
mkubincova <mkubincova@proton.me>
2024-04-24 11:01:50 +0200

    feat: use store values

--- pinia/src/App.vue
+++ pinia/src/App.vue
@@ -4,11 +4,25 @@
             <img src="./assets/pinia-logo.svg" alt="logo">
             <h1>Pinia Tasks</h1>
         </header>
+
+        <div class="task-list">
+            <div v-for="task in taskStore.tasks" :key="task.id">
+                <TaskDetails :task="task" />
+            </div>
+        </div>
     </main>
 </template>
 
 <script>
+    import TaskDetails from './components/TaskDetails.vue'
+    import { useTaskStore } from './stores/TaskStore'
+
     export default {
-        
+        components: { TaskDetails },
+        setup () {
+            const taskStore = useTaskStore()
+
+            return { taskStore }
+        }
     }
 </script>
