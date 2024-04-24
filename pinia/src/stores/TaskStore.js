2ac2e4fc31559b2ec0db1bf1302bbc9340d076f3
mkubincova <mkubincova@proton.me>
2024-04-24 11:21:08 +0200

    feat: getters

--- pinia/src/stores/TaskStore.js
+++ pinia/src/stores/TaskStore.js
@@ -6,5 +6,16 @@ export const useTaskStore = defineStore('taskStore', {
             { id: 1, title: "buy some milk", isFav: false },
             { id: 2, title: "play Gloomhaven", isFav: true }
         ]
-    })
+    }),
+    getters: {
+        favs() {
+            return this.tasks.filter(task => task.isFav);
+        },
+        favCount() {
+            return this.tasks.reduce((p, c) => {
+                return c.isFav ? p + 1 : p;
+            }, 0);
+        },
+        totalCount: (state) => state.tasks.length
+    },
 });
